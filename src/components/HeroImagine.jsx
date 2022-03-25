df2e3a6a92bf6706934a49c65508d08a3b998c16
KhoiNgLe <nguy0572@algonquinlive.com>
2022-03-24 20:23:37 -0400

    add style to Pricing route

--- src/components/HeroImagine.jsx
+++ src/components/HeroImagine.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./HeroImagine.css";
-import Pricing from "./Pricing";
+
 
 const HeroImagine = (props) => {
   return (
@@ -9,7 +9,6 @@ const HeroImagine = (props) => {
       <div className="heading">
         <h1>{props.heading}</h1>
         <p>{props.text}</p>
-        <Pricing/>
       </div>
     </div>
   );
--- src/routes/Pricing.jsx
+++ src/routes/Pricing.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImagine from '../components/HeroImagine'
-
+import PricingCard from '../components/PricingCard'
 
 const Pricing = () => {
   return (
     <div>
         <Navbar/>
         <HeroImagine heading ='PRICING' text='Choose your trip'/>
-        
+        <PricingCard/>
         <Footer/>
 
     </div>
