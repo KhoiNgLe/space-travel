c610c80fb579ef9796733beafb22f8ad358d1ef6
KhoiNgLe <nguy0572@algonquinlive.com>
2022-03-31 21:54:11 -0400

    Add Training route

---
+++ src/routes/Training.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroImagine from '../components/HeroImagine'
+import Navbar from '../components/Navbar'
+
+const Training = () => {
+  return (
+    <div>
+        <Navbar/>
+        <HeroImagine/>
+    </div>
+  )
+}
+
+export default Training
